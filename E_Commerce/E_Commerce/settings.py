b641d52cac9699743ca03b2e7dabe9c219649b7a
Soner Astan <sonerastan@icloud.com>
2024-04-29 15:57:35 +0200

    Commit

--- E_Commerce/E_Commerce/settings.py
+++ E_Commerce/E_Commerce/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "djmoney",
     "debug_toolbar",
     "ShopNest",
+    
 ]
 
 MIDDLEWARE = [
