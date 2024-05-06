23a6a83d446a9b96abd261618709aad5830c6544
Soner Astan <sonerastan@icloud.com>
2024-05-06 13:06:49 +0200

    Added env File

--- E_Commerce/E_Commerce/settings.py
+++ E_Commerce/E_Commerce/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,9 +89,9 @@ WSGI_APPLICATION = "E_Commerce.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerce',
-        'USER': 'ecommerceuser',
-        'PASSWORD': 'DgSD34c5c83BDFGcdrg',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_NAME") ,
+        'PASSWORD': os.getenv("DB_NAME"),
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
